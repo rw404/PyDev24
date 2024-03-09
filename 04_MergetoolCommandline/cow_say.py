fdf2c4aed701412bcc05aade50807f17d1b40779
rw404 <trapeznikovmisha@mail.ru>
2024-03-09 14:00:02 +0300

    cowthink

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -12,6 +12,48 @@ class CowSayShell(cmd.Cmd):
 
         return split_args
 
+    def do_cowthink(self, args):
+        """
+        Cowthink programm
+        Usage: cowthink [message] [-f cowfile] [-e eyes] [-T tongue]
+
+        Params:
+            message[REQUIRED]: string message
+            -f[OPTIONAL]: one of list_cows
+            -e[OPTIONAL]: cow eyes, 2 first letters are used
+            -T[OPTIONAL]: two letter cow's tongue
+        """
+        splitted_args = self.parse_shlex(args)
+
+        cowsay_params = {
+            "message": splitted_args[0],
+            "cow": "default",
+            "eyes": cowsay.Option.eyes,
+            "tongue": cowsay.Option.tongue,
+        }
+
+        if len(splitted_args) > 1:
+            for param, next_param in zip(splitted_args[:-1], splitted_args[1:]):
+                match param:  # noqa: E999 - ignore match case flake8 error
+                    case "-f":
+                        cowsay_params["cow"] = next_param
+                    case "-e":
+                        cowsay_params["eyes"] = next_param
+                    case "-T":
+                        cowsay_params["tongue"] = next_param
+
+        print(cowsay.cowthink(**cowsay_params))
+
+    def complete_cowthink(self, text, line, begidx, endidx):
+        words = (line[:endidx]).split()
+
+        match words[-1]:
+            case "-e":
+                return [eye.eyes for eye in cowsay.COW_OPTIONS.values()]
+            case "-T":
+                return [tongue.tongue for tongue in cowsay.COW_OPTIONS.values()]
+        return None
+
     def do_cowsay(self, args):
         """
         Cowsay programm
